7c17eeb006279e2cdf3c76c15317277b75b480da
Misael Barcenas <mbarcenas@Misaels-Air.attlocal.net>
2023-10-01 11:41:31 -0500

    completed chapter examples

--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -12,6 +12,28 @@ function randomSelection(arr){
  }
  
  //Experiment with the code above. Try to:
- //a) Print 3 random selections from each array.
- //b) Have the code randomly pick one array, and then print 2 random items from it.
- //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+//a) Print 3 random selections from each array.
+console.log(randomSelection(happiness));
+console.log(randomSelection(words));
+console.log(randomSelection(happiness));
+
+//b) Have the code randomly pick one array, and then print 2 random items from it.
+function pickArray() {
+  let randomArray = Math.floor(Math.random()*2)
+  if (randomArray === 0) {
+    return randomSelection(happiness)
+  } else {
+    return randomSelection(words)
+  }
+}
+ 
+console.log(pickArray())
+
+//c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+let newArray = []
+
+for (i = 0; i < 5; i++){
+  newArray.push(pickArray())
+}
+ 
+console.log(newArray)
