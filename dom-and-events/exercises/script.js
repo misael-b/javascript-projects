8df46891ee0c015c3f7ba04243dd3239490c6152
Misael Barcenas <mbarcenas@Misaels-Air.attlocal.net>
2023-10-29 17:21:00 -0500

    completed exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
-}
+    button.addEventListener("click", function () {
+        statusReport.innerHTML = 'Houston, we have liftoff!';
+    });
+
+    missionAbort.addEventListener("mouseover", function () {
+        document.body.style.backgroundColor = 'red';
+    });
+
+    missionAbort.addEventListener("mouseout", function () {
+        document.body.style.backgroundColor = '';
+    });
+
+    missionAbort.addEventListener("click", function () {
+        let userResponce = window.confirm("Are you sure you want to abort the mission?")
+        if (userResponce) {
+            statusReport.innerHTML = "Mission aborted! Space shuttle returning home";
+        };
+        
+    });
+};
 
 window.addEventListener("load", init);
